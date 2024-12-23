2de1081dab1d0c91ab2b9ee8345e377747bbfe28
Henry Price <halrprice@gmail.com>
2024-12-22 19:45:18 -0500

    chore: migrate vectorstore to mongodb atlas (#1)
    * chore: migrate vectorstore to mongodb atlas
    
    * chore: update sample env

--- app/loader.py
+++ app/loader.py
@@ -1,19 +1,34 @@
+import os
 import sys
 
 from dotenv import load_dotenv
-from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_mongodb import MongoDBAtlasVectorSearch
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from pymongo import MongoClient
 
 load_dotenv()
 
 
 def load_data(pdf_path):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-    vector_store = Chroma(
-        embedding_function=embeddings, persist_directory="./chroma_data"
+
+    mongo_client = MongoClient(os.getenv("MONGODB_ATLAS_CLUSTER_URI"))
+
+    DB_NAME = os.getenv("DB_NAME")
+    COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+    ATLAS_VECTOR_SEARCH_INDEX_NAME = os.getenv("ATLAS_VECTOR_SEARCH_INDEX_NAME")
+
+    MONGODB_COLLECTION = mongo_client[DB_NAME][COLLECTION_NAME]
+
+    vector_store = MongoDBAtlasVectorSearch(
+        collection=MONGODB_COLLECTION,
+        embedding=embeddings,
+        index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
+        relevance_score_fn="cosine",
     )
+
     loader = PyPDFLoader(file_path=pdf_path)
     pages = loader.load_and_split()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
