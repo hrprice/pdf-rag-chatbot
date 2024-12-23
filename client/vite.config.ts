0fd29f8fad22a6d0a7df9f430ea7a9b8bc1e581c
Henry Price <halrprice@gmail.com>
2024-12-22 20:42:34 -0500

    chore: update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pdf-rag-chatbot",
+  base: "/pdf-rag-chatbot/",
 });
